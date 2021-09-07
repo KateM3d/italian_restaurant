bfeb1e00e153a6756b2b9767afd33ea8e850ec4f
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-07 17:08:25 -0400

    added animation

---
+++ script.js
@@ -0,0 +1,4 @@
+gsap.to(".one", { duration: 2, delay: 1, x: -400, ease: "power2.inOut", opacity: 0 })
+gsap.from(".two", { duration: 2, delay: 1.2, x: -400, ease: "power2.inOut", opacity: 0 })
+gsap.from('.header', { duration: 2, delay: 2.5, y: 100, opacity: 0 })
+gsap.from('.btn', { duration: 1, delay: 4, opacity: 0, rotation: 720 })
